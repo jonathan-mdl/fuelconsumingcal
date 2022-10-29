b14101d9841ebaacd452cefc74b28c0110694c9e
Jonathan <jnthnwebapps@gmail.com>
2022-10-28 23:57:48 -0300

    packages_added

--- app/src/main/java/com/example/fuelconsumingcal/feature_calculator/presentation/MainActivity.kt
+++ app/src/main/java/com/example/fuelconsumingcal/feature_calculator/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fuelconsumingcal
+package com.example.fuelconsumingcal.feature_calculator.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fuelconsumingcal.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
